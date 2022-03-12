d4999fd298b91430a97df05602b5cd9ae0702903
Luis Montenegro <mon21699@uvg.edu.gt>
2022-03-12 11:42:00 -0600

    agregar vista

---
+++ Vista.java
@@ -0,0 +1,39 @@
+/**
+ * @author Luis Montenegro, Nikolas 
+ * @since 10/03/22
+ * @version 1.0
+ */
+import java.util.Scanner;
+
+public class Vista {
+    private Scanner scan;
+
+    public Vista(){
+        scan = new Scanner(System.in);
+    }
+
+    public void menu(){
+        System.out.println("Ingrese qué tipo de mapa desea usar\n1 HashMap\n2 TreeMap\n3 LinkedHashMap\n4 Salir");
+    }
+
+    public void agregar(){
+        System.out.println("Ingrese la carta que quiere agregar: ");
+    }
+
+    public String input(){
+        return scan.next();
+    }
+
+    public void ioExceptionError(){
+        System.out.println("Error con el archivo");
+    }
+
+    public void cartaNoEncontrada(){
+        System.out.println("ERROR: La carta ingresada no existe");
+    }
+
+    public void finalziar(){
+        System.out.println("Programa finalizado");
+    }
+
+}
